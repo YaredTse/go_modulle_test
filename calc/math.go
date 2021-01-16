29c30df5b8d6d653c46d8f424d0a6c7f401d2094
Yared Tseghu <jaredtse@hotmail.com>
2021-01-16 13:53:35 +0000

    Initial commit.

---
+++ calc/math.go
@@ -0,0 +1,13 @@
+package calc
+
+// returns sum of two integers
+func Add(numbers ...int) int {
+	sum := 0
+
+	for _, num := range numbers {
+		sum = sum + num
+	}
+
+	return sum
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/YaredTse/go_modulle_test
+
+go 1.14
